48c082477a4071f7aee31b1e3ccbd7f3f7827b12
aqnietqaysarbay-ctrl <aqniet.qaysarbay@jihc.edu.kz>
2026-02-02 11:54:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let w = document.querySelector(".w");
+let menu = document.querySelector(".menu");
+w.addEventListener("click", function () {
+  menu.classList.toggle("hide");
+});
+
+let ali = document.querySelector(".ali");
+let menu2 = document.querySelector(".menu2");
+ali.addEventListener("click", function () {
+  menu2.classList.toggle("hide");
+});
